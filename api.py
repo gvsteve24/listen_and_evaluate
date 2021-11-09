21686b5478c977f3844320ed7b4768527615afd1
gvsteve24 <junghyun30124@gmail.com>
2021-11-09 18:41:30 +0900

    video link added, public/index.js refactored

--- api.py
+++ api.py
@@ -3,9 +3,11 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public"), name="public")
 
+app.mount("/", StaticFiles(directory="public"), name="public")
 templates = Jinja2Templates(directory="public")
+
+
 @app.get("/")
 async def home():
-    return templates.TemplateResponse("index.html")
+    return templates.TemplateResponse('index.html')
--- public/index.js
+++ public/index.js
@@ -1,125 +1,142 @@
-const recordButton = document.getElementById("record");
-const playButton = document.getElementById("play");
-const coloredCircle = document.getElementsByClassName("circle")[0];
-const evalButton = document.getElementById("eval");
-
-const initMediaStream = async function() {
-    // returns promise( resolved: MediaStream, failed: NonallowedError / NotFoundError, DOMException )
-    const constraints = {
-        'video': {
-            width: 640,
-            height: 360,
-        },
-        'audio': {
-            echoCancellation: { exact: true },
+class VideoRecorder {
+    constructor() {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+            window.alert("There is no supporting device for recording!")
         }
-    };
-    return await navigator.mediaDevices.getUserMedia(constraints);
-};
+        this.recordButton = document.getElementById("record")
+        this.playButton = document.getElementById("play")
+        this.coloredCircle = document.getElementsByClassName("circle")[0]
+        this.saveButton = document.getElementById("save")
+        this.videoPlayer = document.getElementById('video-player')
 
-const beginRecord = async (onStreamReady, onFinished) => {
-    const stream = await initMediaStream();
+        this.recorder = undefined
+        this.recorded = false
+        this.recordBlobs = []
+        this.savedURL = ''
 
-    onStreamReady(stream);
+        this.recordButton.onclick = this.beginRecord.bind(this)
+        this.playButton.onclick = this.playRecordedBlobs.bind(this)
+        this.saveButton.onclick = this.downloadFile.bind(this)
+        this.coloredCircle.onclick = this.toggleColor.bind(this)
+        this.record = this.record.bind(this)
+        this.detectMimeType = this.detectMimeType.bind(this)
+        this.combineBlobs = this.combineBlobs.bind(this)
+        this.createBlobURL = this.createBlobURL.bind(this)
+        this.stopPlaying = this.stopPlaying.bind(this)
+        this.playStream = this.playStream.bind(this)
+        this.playRecordedBlobs = this.playRecordedBlobs.bind(this)
+        this.setData = this.setData.bind(this)
+        this.stopMediaStream = this.stopMediaStream.bind(this)
 
-    const options = { mimeType: detectMimeType() };
-    const recordBlobs = [];
-    const mediaRecorder = new MediaRecorder(stream, options);
-    mediaRecorder.ondataavailable = (event) => {
-        if (event.data && event.data.size > 0) {
-            recordBlobs.push(event.data);
-        }
-    };
-    mediaRecorder.onstop = () => {
-        onFinished(recordBlobs);
-        stopMediaStream(stream);
-    }
-    mediaRecorder.start();
-    return mediaRecorder;
-}
-
-const detectMimeType = () => {
-    const mimeTypes = [
-        'video/webm;codecs=vp9,opus',
-        'video/webm;codecs=vp8,opus',
-        'video/webm',
-    ];
-    for (let mimeType of mimeTypes) {
-        if (MediaRecorder.isTypeSupported(mimeType)) {
-            return mimeType;
+        this.constraints = {
+            'video': {
+                width: 640,
+                height: 360,
+            },
+            'audio': {
+                echoCancellation: {exact: true},
+            }
         }
     }
-    return '';
-};
 
-const combineBlobs = (recordedBlobs) => {
-    return new Blob(recordedBlobs, { type: 'video/webm' });
-};
-
-const createBlobURL = (blobs) => {
-    const combinedBlob = combineBlobs(blobs);
-    return window.URL.createObjectURL(combinedBlob);
-};
-
-const stopPlaying = (videoElement) => {
-    videoElement.pause();
-    videoElement.src = null;
-    videoElement.srcObject = null;
-};
-
-const playStream = (videoElement, stream) => {
-    stopPlaying(videoElement);
-    videoElement.srcObject = stream;
-    videoElement.play();
-}
-
-const playRecordedBlobs = (videoElement, url) => {
-    stopPlaying(videoElement);
-
-    videoElement.controls = true;
-    videoElement.src = url;
-    videoElement.play();
-};
-
-const setData = (data) => {
-    const url = createBlobURL(data);
-    localStorage.setItem('audio_sample', url);
-    console.log('saved locally.');
-};
-
-const stopMediaStream = async (stream) => {
-    stream.getTracks().forEach((track) => track.stop());
-};
-
-let recorder = undefined;
-let recorded = false;
-document.addEventListener("DOMContentLoaded", function() {
-    const videoElement = document.getElementById('video-player');
-
-    recordButton.addEventListener('click', async function(){
+    async beginRecord() {
         try {
-            if(!recorder){
-                recorder = await beginRecord((stream) =>
-                    playStream(videoElement, stream), (recordedBlobs) => setData(recordedBlobs));
+            if(!this.recorder){
+                this.recorder = await this.record((stream) =>
+                    this.playStream(stream), (recordedBlobs) => this.setData(recordedBlobs));
             }else {
-                recorder.stop();
-                stopPlaying(videoElement);
-                recorder = undefined;
-                recorded = true;
+                this.recorder.stop();
+                this.stopPlaying(this.videoPlayer);
+                this.recorder = undefined;
+                this.recorded = true;
             }
         } catch (err) {
             console.error(err);
         }
-    });
-
-    playButton.addEventListener('click', function(){
-        const savedURL = localStorage.getItem("audio_sample");
-        playRecordedBlobs(videoElement, savedURL);
-    });
-
-    coloredCircle.onclick = function() {
-        coloredCircle.classList.toggle("on-record");
     }
 
-    // To-do: Blob transfer
-});
+    async record(onStreamReady, onFinished) {
+        const stream = await navigator.mediaDevices.getUserMedia(this.constraints);
+
+        onStreamReady(stream);
+
+        const options = { mimeType: this.detectMimeType() };
+        const mediaRecorder = new MediaRecorder(stream, options);
+        mediaRecorder.ondataavailable = (event) => {
+            if (event.data && event.data.size > 0) {
+                this.recordBlobs.push(event.data);
+            }
+        };
+        mediaRecorder.onstop = () => {
+            onFinished(this.recordBlobs);
+            this.stopMediaStream(stream);
+        }
+        mediaRecorder.start();
+        return mediaRecorder;
+    }
+
+    detectMimeType() {
+        const mimeTypes = [
+            'video/webm;codecs=vp9,opus',
+            'video/webm;codecs=vp8,opus',
+            'video/webm',
+        ];
+        for (let mimeType of mimeTypes) {
+            if (MediaRecorder.isTypeSupported(mimeType)) {
+                return mimeType;
+            }
+        }
+        return '';
+    }
+
+    combineBlobs(recordedBlobs) {
+        return new Blob(recordedBlobs, { type: 'video/webm' });
+    }
+
+    createBlobURL(blobs) {
+        const combinedBlob = this.combineBlobs(blobs);
+        return window.URL.createObjectURL(combinedBlob);
+    }
+
+    stopPlaying() {
+        this.videoPlayer.pause();
+        this.videoPlayer.src = null;
+        this.videoPlayer.srcObject = null;
+    }
+
+    playStream(stream) {
+        this.stopPlaying();
+        this.videoPlayer.srcObject = stream;
+        this.videoPlayer.play();
+    }
+
+    playRecordedBlobs() {
+        this.stopPlaying(this.videoPlayer);
+        this.videoPlayer.controls = true;
+        this.videoPlayer.src = this.savedURL;
+        this.videoPlayer.play();
+    }
+
+    setData(data) {
+        this.savedURL = this.createBlobURL(data);
+    }
+
+    async stopMediaStream(stream) {
+        stream.getTracks().forEach((track) => track.stop());
+    }
+
+    async downloadFile() {
+        if (this.savedURL) {
+            this.saveButton.href = this.savedURL;
+            this.saveButton.download = 'video.webm';
+        }else {
+            window.alert("No video exists!");
+        }
+    }
+
+    toggleColor(){
+        this.coloredCircle.classList.toggle("on-record");
+    }
+}
+
+window.videoRecorder = new VideoRecorder();
